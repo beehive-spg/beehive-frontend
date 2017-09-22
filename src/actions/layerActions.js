4967fbe1240eea56b375a1d4b46a0ee03f8a9a5a
astwys <astwys@gmail.com>
2017-09-22 20:32:40 +0200

    DLN-106 add addDrones action

--- src/actions/layerActions.js
+++ src/actions/layerActions.js
@@ -1,10 +1,19 @@
-import { ADD_DRONE } from 'constants/actionTypes'
+import { ADD_DRONE, ADD_DRONES } from 'constants/actionTypes'
 
 export function addDrone(drone) {
 	return {
 		type: ADD_DRONE,
 		payload: {
 			drone,
+		},
+	}
+}
+
+export function addDrones(drones) {
+	return {
+		type: ADD_DRONES,
+		payload: {
+			drones,
 		},
 	}
 }
--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,1 +1,2 @@
 export const ADD_DRONE = 'ADD_DRONE'
+export const ADD_DRONES = 'ADD_DRONES'
