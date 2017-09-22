5411b108e44624b0e3229d5e886b38bfb5fbbc00
astwys <astwys@gmail.com>
2017-09-22 19:43:21 +0200

    DLN-68 #time 30m refactor apollo client code

--- src/index.js
+++ src/index.js
@@ -1,37 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {
-	ApolloClient,
-	ApolloProvider,
-	createNetworkInterface,
-} from 'react-apollo'
-import {
-	SubscriptionClient,
-	addGraphQLSubscriptions,
-} from 'subscriptions-transport-ws'
+import { ApolloProvider } from 'react-apollo'
 import dotenv from 'dotenv'
-import './index.css'
 import Main from './components/main'
+import './index.css'
+import client from './client'
 import registerServiceWorker from './registerServiceWorker'
 
 dotenv.config()
-
-const wsClient = new SubscriptionClient('ws://localhost:8080/subscriptions', {
-	reconnect: true,
-})
-
-const networkInterface = createNetworkInterface({
-	uri: 'http://localhost:8080/graphql',
-})
-
-const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
-	networkInterface,
-	wsClient,
-)
-
-const client = new ApolloClient({
-	networkInterface: networkInterfaceWithSubscriptions,
-})
 
 /* eslint-disable */
 if (module.hot) {
