74023f60a4a5cb34eb1425cc2acf1e65526be2f3
astwys <astwys@gmail.com>
2017-09-22 21:53:32 +0200

    DLN-106 #time 10m add redux action and reducers for adding drones

--- src/actions/layerActions.js
+++ src/actions/layerActions.js
@@ -1,19 +1,48 @@
 import { ADD_DRONE, ADD_DRONES } from 'constants/actionTypes'
 
 export function addDrone(drone) {
+	const { currentPosition } = drone.route
+	const droneLayer = {
+		id: drone.id,
+		data: [
+			{
+				position: [currentPosition.longitude, currentPosition.latitude],
+				radius: 5,
+				color: [36, 184, 41],
+			},
+		],
+	}
+
 	return {
 		type: ADD_DRONE,
 		payload: {
-			drone,
+			droneLayer,
 		},
 	}
 }
 
 export function addDrones(drones) {
+	const droneLayers = drones.map(drone => {
+		const { currentPosition } = drone.route
+		return {
+			id: drone.id,
+			data: [
+				{
+					position: [
+						currentPosition.longitude,
+						currentPosition.latitude,
+					],
+					radius: 5,
+					color: [36, 184, 41],
+				},
+			],
+		}
+	})
+
 	return {
 		type: ADD_DRONES,
 		payload: {
-			drones,
+			droneLayers,
 		},
 	}
 }
