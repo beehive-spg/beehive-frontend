465a0f93f0cfa61172e61d552a4802fd432853d5
astwys <astwys@gmail.com>
2017-10-16 19:46:24 +0200

    DLN-117 #time 5m make creation of layers more dry

--- src/mapbox/createDroneLayers.js
+++ src/mapbox/createDroneLayers.js
@@ -1,9 +1,5 @@
 import { addDroneLayer } from './layers'
 
 export default drones => {
-	const layers = []
-	drones.forEach(drone => {
-		layers.push(addDroneLayer(drone))
-	})
-	return layers
+	return drones.map(drone => addDroneLayer(drone))
 }
