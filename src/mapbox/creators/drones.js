7b6a930be44218e850bc7fad2957336954340629
astwys <astwys@gmail.com>
2017-10-18 22:47:32 +0200

    DLN-118 #time 15m add route and counter for current position to drone data

--- src/mapbox/creators/drones.js
+++ src/mapbox/creators/drones.js
@@ -1,29 +1,21 @@
-export function addDrone(drone) {
-	const { currentPosition } = drone.route
-	return {
-		id: drone.id,
-		data: [
-			{
-				position: [currentPosition.longitude, currentPosition.latitude],
-				radius: 5,
-				color: [36, 184, 41],
-			},
-		],
-	}
-}
+import line from './route'
 
 export function addDrones(drones) {
 	return drones.map(drone => {
-		const { currentPosition } = drone.route
+		const { from, to, currentPosition } = drone.route
+		const route = line(from, to)
+
 		return {
 			id: drone.id,
+			route,
+			counter: 0,
 			data: [
 				{
 					position: [
 						currentPosition.longitude,
 						currentPosition.latitude,
 					],
-					radius: 5,
+					radius: 10,
 					color: [36, 184, 41],
 				},
 			],
