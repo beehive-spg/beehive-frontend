e894ee411763126d439447768120099c3484dceb
Michael Bartl <astwys@gmail.com>
2017-10-31 14:25:47 +0100

    DLN-140 #time 10m add info store to sidebar
    sidebar now uses the redux info store to display drones
    
    Signed-off-by: Michael Bartl <astwys@gmail.com>

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,18 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import Drone from './drone/drone'
 
 import './sidebar.css'
 
+@connect(store => {
+	return {
+		drones: store.info.drones,
+	}
+})
 export default class Sidebar extends React.Component {
 	render() {
-		const { data } = this.props
-		const drones = data.drones.map(drone => <Drone drone={drone} />)
+		const { drones } = this.props
+		const droneComponents = drones.map(drone => <Drone drone={drone} />)
 
 		return (
 			<div className="sidebar">
 				<div className="heading">Drones</div>
 				<hr />
-				<div className="container">{drones}</div>
+				{droneComponents}
 			</div>
 		)
 	}
