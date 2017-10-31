5f0ad0e7031627c5d2aab929e32e11bd707cadb4
Michael Bartl <astwys@gmail.com>
2017-10-31 14:26:57 +0100

    DLN-140 add sidebar component to the main view

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Map from 'components/map/map'
+import Sidebar from 'components/sidebar/sidebar'
 
 export default () => (
 	<div>
 		<Helmet title="Drone Logistics Network" />
 		<Map />
+		<Sidebar />
 	</div>
 )
