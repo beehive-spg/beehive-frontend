79c98c2c33242ee527b2036929cc462037ba2a31
Michael Bartl <astwys@gmail.com>
2017-11-09 19:55:49 +0100

    DLN-176 #time 15m add onclick method and forward if drone is selected

--- src/components/sidebar/droneInfo/droneInfo.js
+++ src/components/sidebar/droneInfo/droneInfo.js
@@ -1,17 +1,42 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { selectDrone } from 'redux/actions/infoActions'
+
 import Drone from './drone/drone'
 
 @connect(store => {
 	return {
 		drones: store.info.drones,
+		selectedDrone: store.info.selectedDrone,
 	}
 })
 export default class DroneInfo extends React.Component {
+	onClick = drone => {
+		const { selectedDrone } = this.props
+		if (drone === selectedDrone) {
+			drone = null
+		}
+
+		this.props.dispatch(selectDrone(drone))
+	}
+
 	render() {
 		const { drones } = this.props
-		const droneComponents = drones.map(drone => <Drone drone={drone} />)
+		const droneComponents = drones.map(drone => {
+			let selected = false
+			if (drone.id === this.props.selectedDrone) {
+				selected = true
+			}
+
+			return (
+				<Drone
+					drone={drone}
+					onClick={this.onClick}
+					selected={selected}
+				/>
+			)
+		})
 
 		return <div>{droneComponents}</div>
 	}
