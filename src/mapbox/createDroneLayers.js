759090db104738f14e32a04e87321dd4176d7239
Michael Bartl <astwys@gmail.com>
2017-11-09 19:54:59 +0100

    DLN-176 #time 10m check for selected drone and change color/radius

--- src/mapbox/createDroneLayers.js
+++ src/mapbox/createDroneLayers.js
@@ -1,11 +1,18 @@
 import { addDroneLayer } from './layers'
 
-export default drones => {
+export default (drones, selectedDrone) => {
 	const data = drones.map(drone => {
+		let color = drone.color
+		let radius = drone.radius
+		if (drone.id === selectedDrone) {
+			color = [16, 150, 18]
+			radius = 50
+		}
+
 		return {
 			position: drone.route[drone.counter].geometry.coordinates,
-			radius: drone.radius,
-			color: drone.color,
+			radius,
+			color,
 		}
 	})
 	return addDroneLayer(data)
