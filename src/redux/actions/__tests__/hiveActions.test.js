5cb74008c8aae78791a9102f6224ff79155bd0f6
Michael Bartl <astwys@gmail.com>
2017-12-05 19:22:33 +0100

    DLN-209 fix action tests

--- src/redux/actions/__tests__/hiveActions.test.js
+++ src/redux/actions/__tests__/hiveActions.test.js
@@ -1,5 +1,5 @@
 import * as types from 'constants/actionTypes'
-import * as actions from '../infoActions'
+import * as actions from '../hiveActions'
 const dispatch = jest.fn()
 
 beforeEach(() => {
