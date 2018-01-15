d3a1e01e93b1d693ca9681ad62acc7a12502d899
Michael Bartl <astwys@gmail.com>
2018-01-15 16:55:06 +0100

    add graphql loader to webpack production build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -216,6 +216,11 @@ module.exports = {
 						),
 						// Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
 					},
+					{
+						test: /\.(graphql|gql)$/,
+						exclude: /node_modules/,
+						loader: 'graphql-tag/loader',
+					},
 					// "file" loader makes sure assets end up in the `build` folder.
 					// When you `import` an asset, you get its filename.
 					// This loader don't uses a "test" so it will catch all modules
