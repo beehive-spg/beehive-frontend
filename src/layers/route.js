1b9801548816229998d125342c1debcb731043a2
Michael Bartl <astwys@gmail.com>
2018-02-14 22:24:27 +0100

    DLN-122 #time 20m add basic route path layer

---
+++ src/layers/route.js
@@ -0,0 +1,34 @@
+import { PathLayer } from 'deck.gl'
+
+const routeLayer = routes => {
+	const data = routes.map(route => {
+		let path = []
+		route.hops.forEach(hop => {
+			const start = [
+				hop.start.location.longitude,
+				hop.start.location.latitude,
+			]
+			const end = [hop.end.location.longitude, hop.end.location.latitude]
+
+			path.push(start)
+			path.push(end)
+		})
+
+		return {
+			path: [...path],
+			width: 10,
+			color: [151, 45, 7],
+		}
+	})
+	return layer(data)
+}
+
+const layer = data => {
+	return new PathLayer({
+		id: 'layer-route',
+		data,
+		rounded: true,
+	})
+}
+
+export default routeLayer
